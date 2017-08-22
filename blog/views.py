74b79edcb54e849b60c8f7ea568c9c1323c03fb8
allonshore <314782553@qq.com>
2017-08-22 14:08:45 +0800

    新增模板标签

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post,Category
 
 def index(request):
     post_list = Post.objects.all().order_by('create_time')
@@ -9,3 +9,14 @@ def index(request):
 def detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/detail.html',context={'post':post})
+
+def archives(request,year,month):
+    post_list = Post.objects.filter(create_time__year=year,
+                                    create_time__month=month
+                                    ).order_by('-create_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
+
+def category(request,pk):
+    cate = get_object_or_404(Category,pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-create_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
