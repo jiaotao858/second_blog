236f4ffdedb47c894e78d68b1d5255da0bc63b6e
allonshore <314782553@qq.com>
2017-08-28 17:52:53 +0800

    通用视图

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 #视图与路由进行绑定
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.IndexView.as_view(),name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$',views.detail,name='detail'),  #文章地址页
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.archives,name='archives'),    #归档地址页
     url(r'^category/(?P<pk>[0-9]+)/$',views.category,name='category'),     #分类地址页
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,18 @@ from django.http import HttpResponse
 from .models import Post,Category
 from comments.forms import CommentForm
 import markdown
+from django.views.generic import ListView  #通用视图类
 
-#   博客首页
-def index(request):
-    post_list = Post.objects.all()
-    return render(request,'blog/index.html',context={'post_list':post_list})
+#   博客首页方法一
+# def index(request):
+#     post_list = Post.objects.all()
+#     return render(request,'blog/index.html',context={'post_list':post_list})
+
+#   博客首页方法二
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
 #   博客详情页
 def detail(request,pk):
@@ -41,8 +48,14 @@ def archives(request,year,month):
                                     )
     return render(request,'blog/index.html',context={'post_list':post_list})
 
-#   博客分类
-def category(request,pk):
-    cate = get_object_or_404(Category,pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request,'blog/index.html',context={'post_list':post_list})
+#   博客分类方法一
+# def category(request,pk):
+#     cate = get_object_or_404(Category,pk=pk)
+#     post_list = Post.objects.filter(category=cate)
+#     return render(request,'blog/index.html',context={'post_list':post_list})
+
+#   博客分类方法二
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
