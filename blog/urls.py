2b44d8bfa9c979dc855516ed8f57d91c166ec94b
allonshore <314782553@qq.com>
2017-09-01 15:37:43 +0800

    完善分页

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ app_name = 'blog'
 urlpatterns = [
     url(r'^$',views.IndexView.as_view(),name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$',views.detail,name='detail'),  #文章地址页
-    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.archives,name='archives'),    #归档地址页
-    url(r'^category/(?P<pk>[0-9]+)/$',views.category,name='category'),     #分类地址页
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.ArchivesView.as_view(),name='archives'),    #归档地址页
+    url(r'^category/(?P<pk>[0-9]+)/$',views.CategoryView.as_view(),name='category'),     #分类地址页
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,34 @@ from django.http import HttpResponse
 from .models import Post,Category
 from comments.forms import CommentForm
 import markdown
-from django.views.generic import ListView  #通用视图类
+from django.views.generic import ListView,DetailView  #通用视图类
 
-#   博客首页方法一
+#   博客首页方法普通方法
 # def index(request):
 #     post_list = Post.objects.all()
 #     return render(request,'blog/index.html',context={'post_list':post_list})
 
-#   博客首页方法二
+#   博客首页方法二：通用视图类
 class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
+    #指定paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章
+    paginate_by = 2
 
-#   博客详情页
+    def get_context_data(self, **kwargs):
+
+        #首先获取父类生成的传递给模板的字典。
+        context = super().get_context_data(**kwargs)
+
+        #父类生成的字典中已有paginator、page_obj、is_paginated
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        pagination_data = self.pagination_data(paginator,page,is_paginated)
+
+  # 博客详情页：普通方法
 def detail(request,pk):
     #   get_object_or_404有就获取，没有就报404
     post = get_object_or_404(Post,pk=pk)
@@ -40,22 +54,41 @@ def detail(request,pk):
                'comment_list':comment_list
                 }
     return render(request,'blog/detail.html',context=context)
+# 博客详情页：通用视图类
 
-#   博客归档
-def archives(request,year,month):
-    post_list = Post.objects.filter(create_time__year=year,
-                                    create_time__month=month
-                                    )
-    return render(request,'blog/index.html',context={'post_list':post_list})
 
-#   博客分类方法一
+#   博客归档一：普通方法
+# def archives(request,year,month):
+#     post_list = Post.objects.filter(create_time__year=year,
+#                                     create_time__month=month
+#                                     )
+#     return render(request,'blog/index.html',context={'post_list':post_list})
+
+
+#   博客归档二：通用视图类
+class ArchivesView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        year =self.kwargs.get('year')
+        month =self.kwargs.get('month')
+        return super(ArchivesView,self).get_queryset().filter(create_time__year=year,
+                                                              create_time__month=month,
+                                                              )
+#   博客分类方法一：普通方法
 # def category(request,pk):
 #     cate = get_object_or_404(Category,pk=pk)
 #     post_list = Post.objects.filter(category=cate)
 #     return render(request,'blog/index.html',context={'post_list':post_list})
 
-#   博客分类方法二
+#   博客分类方法二：通用视图类
 class CategoryView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
+        return super(CategoryView,self).get_queryset().filter(category=cate)
